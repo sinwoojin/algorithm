02a334e26e679538bfd9c24b2f8b875412984266
sinwoojin <slow3993@gmail.com>
2025-03-22 16:12:58 +0900

    [c]c언어 기초

---
+++ 코딩 기초 트레이닝/문자열/a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+
+    unsigend char ch;
+
+    ch = 'A';
+
+    printf("\n\n\nHello World!\n\n\n")
+    printf("\t THe content of ch in decimal is: %d!\n\n\n",ch);
+    printf("\t THe content of ch in hexadecimal is: %x!\n\n\n",ch);
+    printf("\t THe content of ch in octal is: %o!\n\n\n",ch); // octal로 반환
+    printf("\t THe content of ch in character is: %c!\n\n\n",ch); // char로 반환
+}
+
+/*
+    1.
+ */
