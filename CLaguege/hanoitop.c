7dc22fa8a30857b1d5486371faba54334bb58c72
sinwoojin <slow3993@gmail.com>
2025-03-29 16:04:42 +0900

    오타 수정

--- CLaguege/hanoitop.c
+++ CLaguege/hanoitop.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // 함수 선언
-// n : 원판의 갯수, form: 시작 기둥, tmp: 임시기둥, to: 목표기둥
+// n : 원판의 갯수, from: 시작 기둥, tmp: 임시기둥, to: 목표기둥
 void hanoi(int n, char start, char work, char target);
 
 void main(){
@@ -12,19 +12,19 @@ void main(){
 }
 
 // 하노이탑 함수 정의
-// n : 원판의 갯수, form: 시작 기둥, tmp: 임시기둥, to: 목표기둥
+// n : 원판의 갯수, from: 시작 기둥, tmp: 임시기둥, to: 목표기둥
 void hanoi(int n, char from, char tmp, char to){
     // 원판이 하나일 경우, 시작 기둥에서 목표 기둥으로 옮기면 됨
     if( n == 1)
     // 1번 원판을 시작 기둥에서 목표 기둥으로 옮긴다는 메시지 출력
-        printf("\n\n Move plate 1 from %c to %c.\n",form, to);
+        printf("\n\n Move plate 1 from %c to %c.\n",from, to);
     else {
         // 1. 먼저 (n - 1)개의 원판을 from 에서 tmp로 옮긴다
         // tmp는 임시 기둥이므로, 목표 기둥인 to는 대신 사용된다.
-        hanoi( n - 1, form, to, tmp);
+        hanoi( n - 1, from, to, tmp);
 
         // 2. n번째 원판(가장 큰 원판)을 from에서 to로 옮긴다.
-        printf("\n\n Move plate %d from %c to %c.\n", n, form, to);
+        printf("\n\n Move plate %d from %c to %c.\n", n, from, to);
 
         // 3. 마지막으로, (n - 1)개의 원판을 tmp에서 to로 옮긴다.
         // 이제 tmp는 원판들이 이동한 기둥이고, to는 최종 목표 기둥이다.
