89f63a9bde5be54714d9bbb0552f4f1553bb871f
sinwoojin <slow3993@gmail.com>
2025-03-29 13:46:39 +0900

    c언어 출력

--- CLaguege/C언어 기초.c
+++ CLaguege/C언어 기초.c
@@ -2,12 +2,12 @@
 
 void main(){
 
-    unsigend char ch;
+    unsigend char ch; // unsigned 타입 지정을 할거라는 문구
 
     ch = 'A';
 
     printf("\n\n\nHello World!\n\n\n")
-    printf("\t THe content of ch in decimal is: %d!\n\n\n",ch);
+    printf("\t THe content of ch in decimal is: %d!\n\n\n",ch); // 10진수로 변환
     printf("\t THe content of ch in hexadecimal is: %x!\n\n\n",ch);
     printf("\t THe content of ch in octal is: %o!\n\n\n",ch); // octal로 반환
     printf("\t THe content of ch in character is: %c!\n\n\n",ch); // char로 반환
---
+++ CLaguege/fobonacci.c
@@ -0,0 +1,14 @@
+// 파보나치
+
+#include <stdio.h>
+
+void main(){
+
+}
+
+// 4n + 2 비례 곡선
+
+// 함수 f(n)과 g(n)이 주어졌을때, 모든 n >= n0에 대하여 |f(n)| <= c|g(n)|을 만족하는 상수 c와 n0이 존재하면 f(n) = O(g(n))이다
+// 대부분 알고리즘의 복잡도는 nlogn
+
+// 스택 = 저장공간 sessionStorage
