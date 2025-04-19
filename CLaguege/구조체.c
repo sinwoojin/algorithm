738295ff5953797a527128c9520e70cc9ad99de4
sinwoojin <slow3993@gmail.com>
2025-04-19 13:22:25 +0900

    c언어 구조체 잡기 할당법

---
+++ CLaguege/구조체.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct employee { // 구조체형 선언
+        char name[10];
+        int year;
+        int pay;
+}
+
+void main () {
+
+
+    struct employee Kim;
+
+    return 0;
+}
