0a3a66fe1aff6194355a63f91a8c507a554f4961
sinwoojin <slow3993@gmail.com>
2025-04-19 15:45:54 +0900

    연결리스트

---
+++ CLaguege/구조체_만들기.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct ListNode {
+    char data[10];
+    struct ListNode * link;
+} listNode;
+
+typedef struct {
+    listNode * head;
+} linkedList_h;
+
+linkedList_h * createLinkedList_h (void) {
+    linkedList_h * L;
+
+    L = (linkedList_h*)malloc(sizeof(linkedList_h));
+
+    L -> head = NULL;
+    return L;
+}
+
+insertFirst (linkedList_h * L, char *x) {
+    listNode * newNode;
+    newNode = (listNOde*)malloc(sizeof(listNode));
+    strcpy(newNode -> data, x);
+    newNode -> link = L -> head;
+    L -> head = newNode;
+}
+
