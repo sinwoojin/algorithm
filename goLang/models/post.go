ecc2f6529ec44b9512361062f803c2bfa409c2be
sinwoojin <slow3993@gmail.com>
2025-08-14 21:28:29 +0900

    포스트 모델 정의

---
+++ goLang/models/post.go
@@ -0,0 +1,9 @@
+package models
+
+type Post struct {
+	ID            int    `json:"id"`
+	Title         string `json:"title"`
+	Content       string `json:"content"`
+	WriteDate     string `json:"writeDate"`
+	WriteUsername string `json:"writeUsername"`
+}
